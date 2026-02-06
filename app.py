7910396aad418820fe582e934b496efd4840230c
LouisPraise <164245185+LouisPraise@users.noreply.github.com>
2026-02-06 15:57:46 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
-
 import streamlit as st
 import os
-from llama_index.core import StorageContext, load_index_from_storage, Settings
+from llama_index.core import StorageContext, load_index_from_storage
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.groq import Groq
 
-#Streamlit interface
-st.set_page_config(page_title="Andrew Ng AI Tutor", page_icon="🎓")
-st.set_page_config(page_title="", page_icon="🎓")
-st.title("🎓 IA Assistant - My courses")
+st.set_page_config(page_title="My courses assistant", page_icon="🎓")
+st.title("🎓 IA Assistant- My courses")
 
-#My model configuration
-@st.cache_resource 
-def load_models():
-    Settings.embed_model = HuggingFaceEmbedding(
-        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
-    )
-    #My API key protection (I retrieve it from steamlit)
+# my model
+@st.cache_resource
+def load_query_engine():
+    embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    
+    # Protecting my grop key api
     api_key = st.secrets["GROQ_API_KEY"] 
-    Settings.llm = Groq(model="llama3-8b-8192", api_key=api_key)
+    llm = Groq(model="llama3-8b-8192", api_key=api_key)
     
     storage_context = StorageContext.from_defaults(persist_dir="./storage")
-    index = load_index_from_storage(storage_context)
-    return index.as_query_engine(streaming=True)
+    index = load_index_from_storage(storage_context, embed_model=embed_model)
+    
+    return index.as_query_engine(llm=llm, streaming=True)
 
-query_engine = load_models()
 
-# I want my model to remember the history
+query_engine = load_query_engine()
+
+# I'd like the model generate me the history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -34,7 +32,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-    # user interface
+# user interface
 if prompt := st.chat_input("YOUR QUESTION..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
@@ -43,16 +41,15 @@ if prompt := st.chat_input("YOUR QUESTION..."):
     with st.chat_message("assistant"):
         placeholder = st.empty()
         full_response = ""
-        response = query_engine.query(prompt) 
+        response = query_engine.query(prompt)
         
-       
         for chunk in response.response_gen:
             full_response += chunk
-            placeholder.markdown(full_response + "▌") 
+            placeholder.markdown(full_response + "▌")
         
-        placeholder.markdown(full_response) 
+        placeholder.markdown(full_response)
 
-        # references
+        # courses REFERENCES
         with st.expander("📚 REFERENCES"):
             for source in response.source_nodes:
                 file_name = source.node.metadata.get('file_name', 'Source inconnue')
@@ -62,11 +59,3 @@ if prompt := st.chat_input("YOUR QUESTION..."):
                 st.divider()
 
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-
-
-
-
-       
-        
-
-
